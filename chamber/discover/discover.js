04bf8219af1ce0a58886b229635a692b50de1e24
= <andresfm@byui.edu>
2022-02-18 22:33:47 -0700

    discover

---
+++ chamber/discover/discover.js
@@ -0,0 +1,28 @@
+const images= document.querySelectorAll('img');
+
+const options = { threshold: [.5] }
+
+function preloadImage(img){
+    const source = img.getAttribute('data-src');
+    img.src = source;
+
+}
+
+const io = new IntersectionObserver(
+    (entries, io) => {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                return;
+            } else{
+                preloadImage(entry.target)// call a function send in the image tht is currently intersecting
+                io.unobserve(entry.target); // stop observing it
+
+            }
+        //  console.log(entries);
+        });
+    }, options);    
+
+images.forEach(images=> {
+    io.observe(images);
+
+});
