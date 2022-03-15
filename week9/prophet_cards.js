d83e867eceac295f5f2c5f44d378a05b24de0ce2
= <andresfm@byui.edu>
2022-03-14 18:33:57 -0600

    edited

--- week9/prophet_cards.js
+++ week9/prophet_cards.js
@@ -1,60 +1,33 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
-
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
+  .then((response) => response.json())
+  .then((jsonObject) => {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const prophets = jsonObject['prophets'];
-    prophets.forEach(displayProphets)
+    prophets.forEach(displayProphets);
   });
 
-  function displayProphets(prophet) {  // Create elements to add to the document
+  function displayProphets(prophet){
     let card = document.createElement('section');
-    let h2 = document.createElement('h2');    // Change the textContent property of the h2 element to contain the prophet's full name
+    let h2 = document.createElement('h2');
     let dob = document.createElement('p');
     let pob = document.createElement('p');
-    let img = document.createElement ('img');
-
-
-    h2.textContent =`${prophet.name}  ${prophet.lastname}`;
-    dob.textContent = `${prophet.birthdate}`;
-    pob.textContent = `${prophet.birthplace}`;
-    img.setAttribute('src', prophet.image);
-    img.setAttribute('alt', `${prophet.name}  ${prophet.lastname} ${prophet.order}`);
-
-
-    // Add/append the section(card) with the h2 element
+    let img = document.createElement('img')
+    // Update content of h2, dob, pob and append to card
+    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    dob.textContent = `Birthday: ${prophet.birthdate}`;
+    pob.textContent = `Birthplace: ${prophet.birthplace}`;
+    img.setAttribute('src', prophet.imageurl);
+    img.setAttribute('alt', prophet.name);
+    
     card.appendChild(h2);
     card.appendChild(dob);
     card.appendChild(pob);
     card.appendChild(img);
 
-    // Add/append the existing HTML div with the cards class with the section(card)
+    // Add/append card to the existing div.
     document.querySelector('div.cards').appendChild(card);
+
   }
-
-//to clear out cards
-//   let card = document.querySelectorAll('.cards')
-//   card.forEach((item)=>{
-//       item.remove();
-//   })
-
-
-// changing styles in JS 
-
-let change = document.querySelector('.cards');
-change.setAttribute('style', 'background-color: lightBlue;')
-
-
-
-
-// function changeStyles(){
-//     if (window.innerWidth> 880 && window.innerWidth < 1000)}
-
-//event listeners to resize window and load window
-
-window.addEventListener('resize', changeStyles);
-window.addEventListener('load', changeStyles);
+  
